ac03a9981c69be8cc246067377e62d80a4e2e1e8
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-05-22 09:18:04 +0200

    Clean up example application

--- apps/rule/main.cpp
+++ apps/rule/main.cpp
@@ -1,10 +1,34 @@
-/**
- * @file    main.cpp
- * @brief
- * @author  vonhalle
- * @date    2011-07-29
- * @remarks Copyright (c) BBP/EPFL 2005-2011; All rights reserved. Do not distribute without further notice.
+
+/* Copyright (c) 2014, BBP/EPFL
+ *                     Stefan.Eilemann@epfl.ch
+ *
+ * This file is part of Hello (https://github.com/BlueBrain/Hello)
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of Eyescale Software GmbH nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
+
 
 #include <hello/world.h>
 #include <hello/version.h>
@@ -17,30 +41,32 @@ int main( int argc, char *argv[] )
 {
     // Arguments parsing
     po::variables_map vm;
-    po::options_description desc("Allowed options");
+    po::options_description desc( "Supported options" );
     desc.add_options()
-            ("help,h", "Produce help message.")
-            ("version,v", "Show program name/version banner and exit.")
-            ("rev", "Print the SVN revision number")
-            ;
-    po::store(parse_command_line(argc,argv,desc),vm);
-    po::notify(vm);
-    // help
-    if (vm.count("help")) {
+        ( "help,h", "show help message." )
+        ( "version,v", "Show program name/version banner and exit." )
+        ( "rev", "Print the git revision number" );
+    po::store( parse_command_line( argc, argv, desc ), vm );
+    po::notify( vm );
+
+    if( vm.count( "help" ))
+    {
         std::cout << desc << std::endl;
         return EXIT_SUCCESS;
     }
-    // version
-    if (vm.count("version")) {
+
+    if( vm.count( "version" ))
+    {
         std::cout << "HelloRunner version " << hello::Version::getString()
                   << std::endl
-                  << "Copyright (c) BBP/EPFL 2005-2011; All rights reserved. Do "
-                  << "not distribute without further notice." << std::endl;
+                  << "Copyright (c) BBP/EPFL 2014." << std::endl;
         return EXIT_SUCCESS;
     }
-    // rev
-    if (vm.count("rev")) {
-        std::cout << "SVN revision : " << hello::Version::getRevision() << std::endl;
+
+    if( vm.count( "rev" ))
+    {
+        std::cout << "git revision: " << std::hex
+                  << hello::Version::getRevision() << std::endl;
         return EXIT_SUCCESS;
     }
 
