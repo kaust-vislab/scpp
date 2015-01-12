514c56d8195a085929cb56d7f71fc33ce628d8e1
Madhu Srinivasan <madhu.srinivasan@kaust.edu.sa>
2015-01-12 15:30:45 +0300

    Fixed repositoriy urls

--- CMake/Hello.cmake
+++ CMake/Hello.cmake
@@ -1,10 +1,9 @@
 
 
-set(HELLO_REPO_URL https://github.com/BlueBrain/Hello.git)
-set(HELLO_DEPENDS bluebrain REQUIRED Boost)
+set(HELLO_REPO_URL https://github.com/kaust-vislab/scpp.git)
+set(HELLO_DEPENDS bluebrain REQUIRED Boost SuperCollider)
 set(HELLO_BOOST_COMPONENTS "unit_test_framework program_options")
 set(HELLO_DEB_DEPENDS libboost-test-dev libboost-program-options-dev)
-
 if(CI_BUILD_COMMIT)
   set(HELLO_REPO_TAG ${CI_BUILD_COMMIT})
 else()
